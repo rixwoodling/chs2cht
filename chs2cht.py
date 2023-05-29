d84607f57e1cfebb6b41aea3be5aeb89baa54c7c
rix <rixwoodling@gmail.com>
2023-05-29 09:19:40 -0700

    Add functionality to convert chs to cht

--- chs2cht.py
+++ chs2cht.py
@@ -1,19 +1,34 @@
-#!/bin/env python
+#!/bin/env python3
 
 import csv
+import sys
 
 # convert csv to dictionary
 with open( 'chs2cht.csv','r' ) as csvfile:
     reader = csv.reader( csvfile )
     mydict = { rows[0]:rows[1] for rows in reader }
 
-infile = open( 'subtitles.chs.srt','r' )
-outfile = open( 'subtitles.cht.srt','w' )
+# loop through each .srt file and create output file
+for i in range( 1, len( sys.argv )):
+    if len( sys.argv ) > 0 and sys.argv[i].endswith( '.srt' ): 
+        infile = open( sys.argv[i],'r' )
+        outfile = open( sys.argv[i].replace( '.srt','.cht.srt' ),'w' )
 
-for line in infile:
-    outfile.write( line )
+        for line in infile:
+            for e in line:
+                if e in mydict:
+                    e = e.replace( e, mydict[e] )
+                    print( e,end='' )
+                outfile.write( e )
 
-infile.close()
-outfile.close()
+        infile.close()
+        outfile.close()
+
+        print( "done" )
+
+    else:
+        print( "not an srt file" )
+
+
 
 
